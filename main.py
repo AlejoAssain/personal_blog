ab903450ee86558fc8e2c0ab8498b6df855b63d6
alejo desktopm <alejoassain00@gmail.com>
2021-09-27 14:02:39 -0300

    set up postgres db

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from os import abort
+import os
 from warnings import filterwarnings
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
@@ -15,7 +15,7 @@ from flask_gravatar import Gravatar
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -25,8 +25,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+## CONNECT TO DB
+# the second route is an alternative, when running locally
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
